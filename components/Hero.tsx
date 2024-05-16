19d605d026630460b27d4efe710a5c15db0fb31b
putraprdn <80741806+putraprdn@users.noreply.github.com>
2024-05-16 14:36:49 +0700

    chore: minor changes in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,12 +6,10 @@ const Hero = () => {
 	return (
 		<section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
 			<div className="hero-map" />
-
-			{/* LEFT */}
-
+			
 			<div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
 				<Image
-					src={"/camp.svg"}
+					src="/camp.svg"
 					alt="camp"
 					width={50}
 					height={50}
@@ -29,11 +27,11 @@ const Hero = () => {
 					<div className="flex items-center gap-2">
 						{Array(5)
 							.fill(1)
-							.map((_, i) => (
+							.map((_, index) => (
 								<Image
-									src={"/star.svg"}
-									key={i}
-									alt="Start"
+									src="/star.svg"
+									key={index}
+									alt="star"
 									width={24}
 									height={24}
 								/>
@@ -56,7 +54,7 @@ const Hero = () => {
 					/>
 					<Button
 						type="button"
-						title="How We Work"
+						title="How we work?"
 						icon="/play.svg"
 						variant="btn_white_text"
 					/>
@@ -64,18 +62,17 @@ const Hero = () => {
 			</div>
 
 			<div className="relative flex flex-1 items-start">
-				<div className="relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-					<div className="flex flex-col ">
+				<div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+					<div className="flex flex-col">
 						<div className="flexBetween">
 							<p className="regular-16 text-gray-20">Location</p>
 							<Image
-								src={"/close.svg"}
-								alt="Close"
-								height={24}
+								src="/close.svg"
+								alt="close"
 								width={24}
+								height={24}
 							/>
 						</div>
-
 						<p className="bold-20 text-white">Aguas Calientes</p>
 					</div>
 
@@ -86,7 +83,6 @@ const Hero = () => {
 							</p>
 							<p className="bold-20 text-white">173.28 mi</p>
 						</div>
-
 						<div className="flex flex-col">
 							<p className="regular-16 block text-gray-20">
 								Elevation
